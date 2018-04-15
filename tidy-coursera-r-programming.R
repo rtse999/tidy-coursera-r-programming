7ff512c92fff85644c93f7f6063990e758955e7b
rtse999 <rtse999@gmail.com>
2018-04-15 22:56:52 +1000

    22:56 - Sunday 15 April 2018

---
+++ tidy-coursera-r-programming.R
@@ -0,0 +1,68 @@
+# ------------------------------------------------------------------------
+# Coursera R-Programming: Week 2 Problems using Tidyverse
+# Link: http://josiahparry.com/post/tidy-coursera-r-programming/
+#
+# Location: /Users/raymondtse/Dropbox/Analysis/Misc/tidy-coursera-r-programming.r
+# First created: 22:25 - Sunday 15 April 2018
+# Last modified: 22:25 - Sunday 15 April 2018
+# ------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------
+# System time 
+# ------------------------------------------------------------------------
+format(Sys.time(), "%a %b %d %H:%M:%S %Y")
+
+# ------------------------------------------------------------------------
+# Session Info
+# ------------------------------------------------------------------------
+devtools::session_info()
+
+# ------------------------------------------------------------------------
+# Libraries
+# ------------------------------------------------------------------------
+library(tidyverse)
+
+
+
+# ------------------------------------------------------------------------
+# Problem 1
+# ------------------------------------------------------------------------
+
+pollutantmean <- function(directory, pollutant, id = 1:332){
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the 
+  ## mean; either "sulfate" or "nitrate"
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
+  ## NOTE: Do not round the result!
+}
+
+# 10 random IDs in ID range
+ids <- sample(1:332, 10)
+
+# Identify all files within directory
+files <- list.files("specdata", full.names = TRUE)
+
+# Subset the data
+files_filtered <- files[ids]
+
+# View the files to verify
+paste(ids, files_filtered)
+
+# Read in the subset of data
+specdata <- map_df(files_filtered, read_csv,
+                   col_types = list(
+                     col_date(),
+                     col_double(),
+                     col_double(),
+                     col_integer()
+                   ))
+glimpse(specdata)
+
